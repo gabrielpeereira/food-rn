78f0b26bea4f23607811fc9049a75486bf795c46
Gabriel Pereira <gabriel.developer@outlook.com>
2020-07-10 18:00:33 -0300

    novas telas

---
+++ src/components/ResultsList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ResultsList = () => {
+    return(
+        <View>
+            <Text>Results List</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({});
+
+
+export default ResultsList;
